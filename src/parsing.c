9125865a0e5ed720b6f48f9581418f2153820f64
FrenchKrab <krabygames@gmail.com>
2019-10-24 13:03:53 +0200

    Fait clean_str()

--- src/parsing.c
+++ src/parsing.c
@@ -13,11 +13,87 @@ char* clean_str(char* str) {
     // Supprimer les blancs au début et à la fin de la ligne
     // Ex: "   str1    str2    " => "str1    str2"
 
+
+    int strLen = strlen(str);
+    int trueStartIndex = 0;
+    for (int i = 0; i<strLen; ++i)
+    {
+        if(str[i] == ' ')
+        {
+            trueStartIndex = i;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    //+1 pour prendre le \0 de fin de chaine
+    memmove(str, str + trueStartIndex, strLen - trueStartIndex + 1);
+    strLen = strlen(str);
+
+    for(int i = strLen - 1; i>=0; --i)
+    {
+        if(str[i]==' ' || str[i]=='\n')
+        {
+            str[i]='\0';
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // Etat actuel : "str1    str2"
+
     // Supprimer les doublons d'espaces entre les mots de la ligne
     // Ex: "str1    str2  str3" => "str1 str2 str3"
 
-    return str;
+    /* Methode pas opti
+    strLen = strlen(str);
+    for(int i = 0; i < strLen - 1; ++i)
+    {
+        if(str[i]==' ' && str[i+1]==' ')
+        {
+            //+2 pour prendre le \0 de fin de chaine
+            memmove(str[i],str[i+1], strLen-i+2);
+            strLen = strlen(str);
+        }
     }
+    */
+
+   //methode opti mais qui marche peut etre pas
+    int start = 0;
+    int end = 0;
+    int previousWasSpace = 0;
+    for(int i = 0; i < strLen; ++i)
+    {
+        if(str[i]==' ')
+        {
+            if(previousWasSpace)
+            {
+                end = i;
+            }
+            else
+            {
+                start = i;
+            }
+            previousWasSpace = 1;
+        }
+        else
+        {
+            if(previousWasSpace)
+            {
+                memmove(str[start],str[end], strLen-end+1);
+                i = start + 1;
+            }
+            previousWasSpace = 0;
+        }
+    }
+    
+
+    return str;
+}
 
 
 // Découper la chaîne de caractères en ses différents éléments
